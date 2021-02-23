c8dc8ba5a927baa736619630b1f7290c9a8f6f07
Chitrank Dixit <chitrankdixit@gmail.com>
2021-02-23 22:09:14 +0530

    add test for sum of digits program

---
+++ tests/dcp/test_sum_of_digits.py
@@ -0,0 +1,9 @@
+import pytest
+
+from dcp.sum_of_digits import sum_of_digits
+
+
+class TestDay1:
+    @pytest.mark.parametrize("array,output", [(1052787, 30)])
+    def test_day1(self, array, output):
+        assert sum_of_digits(n=array) == output
