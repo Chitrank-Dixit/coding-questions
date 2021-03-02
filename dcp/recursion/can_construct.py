6e3476732c3f418764f5925ae2105b830d55277b
Chitrank Dixit <chitrankdixit@gmail.com>
2021-03-03 00:28:32 +0530

    add can_construct recursive and dp memoization solutions

---
+++ dcp/recursion/can_construct.py
@@ -0,0 +1,36 @@
+"""
+write a function can_construct(target, word_bank) that accepts a target string and an array of strings
+
+the function should return a boolean indicating whether or not the target can be constructed by concatenating element of
+word_bank array
+
+you may reuse element of word_bank as many times as needed
+"""
+
+
+def can_construct(target, word_bank):
+    if target == "":
+        return True
+    for word in word_bank:
+        try:
+            is_prefix = target.index(word)
+        except ValueError:
+            is_prefix = -1
+
+        if is_prefix == 0:
+            suffix = target[len(word) :]
+            if can_construct(suffix, word_bank):
+                return True
+
+    return False
+
+
+if __name__ == "__main__":
+    print(can_construct("abcdef", ["ab", "abc", "cd", "def", "abcd"]))
+
+    print(
+        can_construct(
+            "eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef",
+            ["ee", "eeeee", "eeeeee", "eeee"],
+        )
+    )
