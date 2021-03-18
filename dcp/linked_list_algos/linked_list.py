1ee83ec6a4991dc21640711a95668ca237c4062e
Chitrank Dixit <chitrankdixit@gmail.com>
2021-03-18 12:40:33 +0530

    add linked list and respective operations of linked list

--- dcp/linked_list_algos/linked_list.py
+++ dcp/linked_list_algos/linked_list.py
@@ -23,10 +23,67 @@ class LinkedList:
         return self.data
 
 
-def traverse(head):
-    while head:
-        print(head)
-        head = head.next_node
+class LinkedListOperations:
+    def __init__(self, head=None):
+        self.head = head
+
+    def traverse(self):
+        temp = self.head
+        while temp:
+            print(temp)
+            temp = temp.next_node
+
+    def add_at_first(self, val):
+        to_add = LinkedList(val)
+        to_add.next_node = self.head
+        self.head = to_add
+
+    def add_at_last(self, val):
+        temp = self.head
+        while temp:
+            if temp.next_node is None:
+                break
+            temp = temp.next_node
+        add_last = LinkedList(val)
+        temp.next_node = add_last
+
+    def add_at_index(self, val, index):
+        temp = self.head
+        count = 0
+        while temp:
+            if count == index - 1:
+                temp1 = temp.next_node
+                add_index = LinkedList(val)
+                temp.next_node = add_index
+                add_index.next_node = temp1
+                break
+
+            temp = temp.next_node
+            count += 1
+
+    def delete_at_first(self):
+        if self.head.next_node is not None:
+            self.head = self.head.next_node
+
+    def delete_at_last(self):
+        temp = self.head
+        while temp:
+            if temp.next_node.next_node is None:
+                break
+            temp = temp.next_node
+        temp.next_node = None
+
+    def delete_at_index(self, index):
+        temp = self.head
+        count = 0
+        while temp:
+            if count == index - 1:
+                temp1 = temp.next_node
+                temp.next_node = temp1.next_node
+                break
+
+            temp = temp.next_node
+            count += 1
 
 
 if __name__ == "__main__":
@@ -46,4 +103,23 @@ if __name__ == "__main__":
     ll2.next_node = ll3
     ll1.next_node = ll2
 
-    traverse(ll1)
+    lo = LinkedListOperations(head=ll1)
+    lo.traverse()
+    print("-----------------")
+    lo.add_at_first("ba")
+    lo.traverse()
+    print("-----------------")
+    lo.add_at_last("vf")
+    lo.traverse()
+    print("-----------------")
+    lo.add_at_index("gh", 2)
+    lo.traverse()
+    print("-----------------")
+    lo.delete_at_first()
+    lo.traverse()
+    print("-----------------")
+    lo.delete_at_last()
+    lo.traverse()
+    print("-----------------")
+    lo.delete_at_index(index=2)
+    lo.traverse()
