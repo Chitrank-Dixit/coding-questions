ac8c578921f8b2cd5d0c73a633a1bcab074e9800
Chitrank Dixit <chitrankdixit@gmail.com>
2021-03-20 09:29:14 +0530

    add binary search iterative approach

--- dcp/searching/binary_search.py
+++ dcp/searching/binary_search.py
@@ -29,6 +29,24 @@ def binary_search_v1(arr, left, right, search_term):
         return -1
 
 
+def binary_search_v2(arr, search_term):
+    low = 0
+    high = len(arr) - 1
+    while low <= high:
+        mid = (low + high) // 2
+        guess = arr[mid]
+        if guess == search_term:
+            return mid
+        if guess > search_term:
+            high = mid - 1
+        else:
+            low = mid + 1
+    return None
+
+
 if __name__ == "__main__":
     arr = [1, 4, 2, 3, 5, 8, 6, 7]
     print(binary_search_v1(arr=arr, left=0, right=len(arr) - 1, search_term=8))
+
+    arr = [1, 4, 2, 3, 5, 8, 6, 7]
+    print(binary_search_v2(arr=arr, search_term=8))
