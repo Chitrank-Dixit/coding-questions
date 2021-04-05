3e9e76f8961fb01fe50fbf72732ab3ec06712b09
Chitrank Dixit <chitrankdixit@gmail.com>
2021-04-06 01:33:44 +0530

    add root to leaf path sum , min path and max path sum of tree solutions

---
+++ dcp/trees_algos/root_to_leaf_path_sum.py
@@ -0,0 +1,26 @@
+class BinaryTree:
+    def __init__(self, val, right=None, left=None):
+        self.val = val
+        self.right = right
+        self.left = left
+
+
+def path_sum(root):
+    if not root:
+        return 0
+    if not root.left and not root.right:
+        return root.val
+
+    return root.val + path_sum(root.left) + path_sum(root.right)
+
+
+if __name__ == "__main__":
+    root = BinaryTree(1)
+    root.left = BinaryTree(2)
+    root.right = BinaryTree(3)
+    root.left.left = BinaryTree(4)
+    root.left.right = BinaryTree(5)
+    root.right.left = BinaryTree(6)
+    root.right.right = BinaryTree(7)
+
+    print(path_sum(root))
