6876e4594df56f8c547310ad6cecb513b3e69024
Chitrank Dixit <chitrankdixit@gmail.com>
2021-04-25 17:05:18 +0530

    add solution for sort list of strings by length

---
+++ oracle/sort_by_string_length.py
@@ -0,0 +1,31 @@
+def printArraystring(string, n):
+    for i in range(n):
+        print(string[i], end=" ")
+
+
+# Function to Sort the array of string
+# according to lengths. This function
+# implements Insertion Sort.
+def sort(s, n):
+    for i in range(1, n):
+        temp = s[i]
+
+        # Insert s[j] at its correct position
+        j = i - 1
+        while j >= 0 and len(temp) < len(s[j]):
+            s[j + 1] = s[j]
+            j -= 1
+
+        s[j + 1] = temp
+
+
+# Driver code
+if __name__ == "__main__":
+    arr = ["GeeksforGeeks", "I", "from", "am"]
+    n = len(arr)
+
+    # Function to perform sorting
+    sort(arr, n)
+
+    # Calling the function to print result
+    printArraystring(arr, n)
