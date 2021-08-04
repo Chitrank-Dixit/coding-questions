c94fd7c587b3469603c9bf76cc6f665ca0b8ded1
Chitrank Dixit <chitrankdixit@gmail.com>
2021-08-05 00:32:26 +0530

    add optimized dfs based solution for day24 must do questions

--- must_do_questions/day24.py
+++ must_do_questions/day24.py
@@ -1,5 +1,5 @@
 """
-Given a binary tree, imagine you're stading to the right of the tree. Return an array of the values of the nodes you can
+Given a binary tree, imagine you're standing to the right of the tree. Return an array of the values of the nodes you can
 see ordered from top to bottom.
 """
 
@@ -26,10 +26,27 @@ def order_nodes(root, level, right_most):
         right_most[level] = None
         left = order_nodes(root.left, level=level, right_most=right_most)
         right = order_nodes(root.right, level=level, right_most=right_most)
-        combined = right.copy()  # start with keys and values of x
+        combined = right.copy()
         combined.update(left)
         return combined
     return right_most
+
+
+def dfs(root, current_level, result):
+    if not root:
+        return result
+    if current_level >= len(result):
+        result.append(root.root)
+    if root.right:
+        dfs(root.right, current_level + 1, result)
+    if root.left:
+        dfs(root.left, current_level + 1, result)
+
+
+def right_side_view(root):
+    result = list()
+    dfs(root, 0, result)
+    return result
 
 
 if __name__ == "__main__":
@@ -41,3 +58,12 @@ if __name__ == "__main__":
     two_2 = Tree(root=22)
     one_1 = Tree(root=11, left=two_1, right=two_2)
     print(order_nodes(one_1, level=1, right_most={0: one_1.root, 1: None}))
+
+    five_1 = Tree(root=51)
+    four_1 = Tree(root=41, right=five_1)
+    three_1 = Tree(root=31, right=four_1)
+    three_2 = Tree(root=32)
+    two_1 = Tree(root=21, left=three_1, right=three_2)
+    two_2 = Tree(root=22)
+    one_1 = Tree(root=11, left=two_1, right=two_2)
+    print(right_side_view(one_1))
