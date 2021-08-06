ccae503d90c882a199d8ace70673f58fd06e5535
Chitrank Dixit <chitrankdixit@gmail.com>
2021-08-07 00:42:05 +0530

    add folder for pivotal

---
+++ pivotal/step_word.py
@@ -0,0 +1,15 @@
+"""
+A step word is formed by taking a given word, adding a letter, and anagramming the result. For example, starting with
+the word "APPLE", you can add an "A" and anagram to get "APPEAL".
+
+Given a dictionary of words and an input word, create a function that returns all valid step words.
+"""
+
+
+def step_word(word):
+    pass
+
+
+if __name__ == "__main__":
+    word = "apple"
+    print(step_word(word))
