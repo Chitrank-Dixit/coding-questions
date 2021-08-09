83faf8435e4000d8a29ecff7697a6690777eab7d
Chitrank Dixit <chitrankdixit@gmail.com>
2021-08-10 00:39:26 +0530

    added solution for fixed_point problem of apple

---
+++ apple/fixed_point.py
@@ -0,0 +1,21 @@
+"""
+A fixed point in an array is an element whose value is equal to its index. Given a sorted array of distinct elements,
+return a fixed point, if one exists. Otherwise, return False.
+
+For example, given [-6, 0, 2, 40], you should return 2. Given [1, 5, 7, 8], you should return False.
+"""
+
+
+def fixed_point(arr):
+    for i in range(len(arr)):
+        if arr[i] == i:
+            return i
+    return False
+
+
+if __name__ == "__main__":
+    arr = [-6, 0, 2, 40]
+    print(fixed_point(arr))
+
+    arr = [1, 5, 7, 8]
+    print(fixed_point(arr))
