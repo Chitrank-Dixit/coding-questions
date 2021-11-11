4ce3963a3c32644154eb41e021394f4164483dd0
Chitrank Dixit <chitrankdixit@gmail.com>
2021-11-11 21:49:03 +0530

    add solution for must do question day 33

--- must_do_questions/day33.py
+++ must_do_questions/day33.py
@@ -22,7 +22,25 @@ def bfs(graph):
     return answer
 
 
+def traversal_bfs(graph):
+    queue = [0]
+    values = list()
+    seen = dict()
+    while len(queue) > 0:
+        vertex = queue.pop(0)
+        values.append(vertex)
+        seen[vertex] = True
+        connections = graph[vertex]
+        for i in range(len(connections)):
+            connection = connections[i]
+            if connection not in seen:
+                queue.append(connection)
+    return values
+
+
 if __name__ == "__main__":
     graph = [[1, 3], [0], [3, 8], [0, 2, 5, 4], [3, 6], [3], [4, 7], [6], [2]]
 
     print(bfs(graph=graph))
+
+    print(traversal_bfs(graph=graph))
