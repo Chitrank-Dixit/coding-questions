5650b5eee6146df37a68c921d8ff2769fac1fb3e
Chitrank Dixit <chitrank.dixit@yougov.com>
2021-11-16 22:38:34 +0530

    Add one more solution for tower of hanoi problem

--- dcp/recursion/towers_of_hanoi.py
+++ dcp/recursion/towers_of_hanoi.py
@@ -14,5 +14,17 @@ def towers_of_hanoi(number_of_disks, start_peg=1, end_peg=3):
         towers_of_hanoi(number_of_disks - 1, 6 - start_peg - end_peg, end_peg)
 
 
+def tower_of_hanoi_v1(s, d, h, n):
+    if n == 1:
+        print(f"Moving plate {n} from {s} to {d}")
+        return
+
+    tower_of_hanoi_v1(s, h, d, n - 1)
+    print(f"Moving plate {n} from {s} to {d}")
+    tower_of_hanoi_v1(h, d, s, n - 1)
+
+
 if __name__ == "__main__":
     towers_of_hanoi(number_of_disks=4)
+
+    tower_of_hanoi_v1(s=1, h=2, d=3, n=4)
