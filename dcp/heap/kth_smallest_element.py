da3515bf3866515be5f199c8b7de28719bf0110f
Chitrank Dixit <chitrank.dixit@yougov.com>
2021-11-21 19:29:37 +0530

    add solution for kth smallest element using heap

---
+++ dcp/heap/kth_smallest_element.py
@@ -0,0 +1,19 @@
+import heapq
+
+
+def kthSmallest(arr, k):
+    smallest = []
+    for value in arr:
+        if len(smallest) < k:
+            heapq.heappush(smallest, -value)
+        else:
+            heapq.heappushpop(smallest, -value)
+    if len(smallest) < k:
+        return None
+    return -smallest[0]
+
+
+if __name__ == "__main__":
+    arr = [1, 5, 2, 6, 3, 8]
+    k = 4
+    print(kthSmallest(arr, k))
