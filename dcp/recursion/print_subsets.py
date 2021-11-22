e4eb6b70c058e70a04d6aa4f298d070e2556c5a0
Chitrank Dixit <chitrank.dixit@yougov.com>
2021-11-22 21:47:46 +0530

    add solution for print subsets

--- dcp/recursion/print_subsets.py
+++ dcp/recursion/print_subsets.py
@@ -6,15 +6,20 @@ print subsets
 def subsets(ip, op):
     if len(ip) == 0:
         print(op, end=" ")
+        return
     op1 = op
     op2 = op
-    op = op.replace(ip[0], "")
-    ip = ip.replace(ip[0], "")
+    op2 += ip[0]
+    ip = ip[1:]
     subsets(ip, op1)
     subsets(ip, op2)
 
 
 if __name__ == "__main__":
     ip = "ab"
+    op = ""
+    print(subsets(ip, op))
+
+    ip = "abcde"
     op = ""
     print(subsets(ip, op))
