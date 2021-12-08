bbc1af25555fc6b015ada28acfd6425d764c1916
Chitrank Dixit <chitrank.dixit@yougov.com>
2021-12-09 01:57:18 +0530

    add recursive solution for josephus problem

---
+++ dcp/recursion/josephus_problem.py
@@ -0,0 +1,31 @@
+"""
+josephus problem
+"""
+
+
+def josephus(v, k, index, ans):
+    if len(v) == 1:
+        print(v[0])
+        return
+
+    index = (index + k) % (len(v))
+    v.pop(index)
+    josephus(v, k, index, ans)
+    return
+
+
+def main():
+    n = 40
+    k = 7
+    index = 0
+    ans = -1
+    v = []
+    for i in range(1, n + 1):
+        v.append(i)
+    k -= 1
+    josephus(v, k, index, ans)
+    return
+
+
+if __name__ == "__main__":
+    print(main())
