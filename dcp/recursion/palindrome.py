b728484ac6275a0cc570554b400007559f861742
Chitrank Dixit <chitrank.dixit@yougov.com>
2021-12-18 00:42:50 +0530

    add recursive solution for checking palindrome number

--- dcp/recursion/palindrome.py
+++ dcp/recursion/palindrome.py
@@ -4,5 +4,19 @@ def is_palindrome(S, i, j):
     return S[i] == S[j] and is_palindrome(S, i + 1, j - 1)
 
 
-S = "xyyzzpzzyyx"
-print(is_palindrome(S, 0, len(S) - 1))
+def is_palindrome_v1(ip):
+    if len(ip) == 0 or len(ip) == 1:
+        return True
+
+    if ip[0] == ip[len(ip) - 1]:
+        return is_palindrome_v1(ip[1 : len(ip) - 1])
+
+    return False
+
+
+if __name__ == "__main__":
+    S = "xyyzzpzzyyx"
+    print(is_palindrome(S, 0, len(S) - 1))
+
+    s = "kayak"
+    print(is_palindrome_v1(s))
