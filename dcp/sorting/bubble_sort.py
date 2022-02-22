82c814635d9d33443673bdb7feca0ad7f82809ec
Chitrank-Dixit <chitrankdixit@gmail.com>
2022-02-22 23:42:29 +0530

    add enhacement for bubble sort

--- dcp/sorting/bubble_sort.py
+++ dcp/sorting/bubble_sort.py
@@ -12,7 +12,28 @@ def bubble_sort(sort):
     return sort
 
 
+def bubble_sort_v1(sort):
+    temp = 0
+    for i in range(0, len(sort)):
+        is_swapped = False
+        for j in range(
+            len(sort) - 1, i, -1
+        ):  # here I have used reverse range range(para1,para2,para3)
+            if sort[j] < sort[j - 1]:
+                temp = sort[j]
+                sort[j] = sort[j - 1]
+                sort[j - 1] = temp
+                is_swapped = True
+        if not is_swapped:
+            break
+    return sort
+
+
 if __name__ == "__main__":
     to_sort = [6, 5, 4, 3, 2, 1]
     sorted_list = bubble_sort(to_sort)
+    print("Sorted list is: ", sorted_list)
+
+    to_sort = [6, 5, 4, 3, 2, 1]
+    sorted_list = bubble_sort_v1(to_sort)
     print("Sorted list is: ", sorted_list)
