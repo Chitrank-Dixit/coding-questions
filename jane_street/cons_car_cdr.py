04eadcc7e86f4f4afae18218bef3544a96eac899
Chitrank-Dixit <chitrankdixit@gmail.com>
2024-08-01 00:43:15 +0530

    add some more solutions

--- jane_street/cons_car_cdr.py
+++ jane_street/cons_car_cdr.py
@@ -11,3 +11,23 @@ def cons(a, b):
     return pair
 Implement car and cdr.
 """
+
+def cons(a, b):
+    def pair(f):
+        return f(a, b)
+    return pair
+
+def car(pair):
+    def return_first(a, b):
+        return a
+    return pair(return_first)
+
+def cdr(pair):
+    def return_last(a, b):
+        return b
+    return pair(return_last)
+
+if __name__ == '__main__':
+    print(car(cons(3, 4)))
+    print(cdr(cons(3, 4)))
+
