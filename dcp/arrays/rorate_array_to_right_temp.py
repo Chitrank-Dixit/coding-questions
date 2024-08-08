3cdaf2a44ad1bcfaa5473d45b42451e46c4d4995
Chitrank-Dixit <chitrankdixit@gmail.com>
2024-08-08 16:33:29 +0530

    add solution for array rotations

---
+++ dcp/arrays/rorate_array_to_right_temp.py
@@ -0,0 +1,16 @@
+# Python code to implement the above approach
+def rotate(L, d, n):
+    new_lis = []
+    new_lis = L[len(L) - d: len(L)] + L[0:len(L) - d]
+    return new_lis
+
+
+if __name__ == '__main__':
+    arr = [1, 7, 3, 4, 5, 6]
+    d = 2
+    N = len(arr)
+
+    # Function call
+    arr = rotate(arr, d, N)
+    for i in arr:
+        print(i, end=" ")
